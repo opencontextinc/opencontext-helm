0b74073cde368bd828d31fbc65317d0935aff245
Alice Chen <alice@opencontext.com>
2023-04-11 15:19:41 -0700

    chore: Remove unneeded files from gh-pages

--- charts/opencontext/files/create-backend-dbs.sql
+++
@@ -1,8 +0,0 @@
-{{ $user := .Values.global.postgresql.auth.username }}
-
-GRANT USAGE, CREATE ON SCHEMA public TO {{ $user }};
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO {{ $user }};
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO {{ $user }};
-GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO {{ $user }};
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE ON SEQUENCES TO  {{ $user }};
-CREATE database {{ $user }};
