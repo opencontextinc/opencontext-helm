e248721ab4e3621303d108dd3002d4d57b068cf9
Alice Chen <alice@opencontext.com>
2023-04-12 19:00:47 -0700

    chore(ci): Update uninstall script

--- .github/uninstall-chart.sh
+++ .github/uninstall-chart.sh
@@ -5,9 +5,10 @@ RELEASE_NAME=${2-oc1}
 echo "INFO: Uninstalling release ${RELEASE_NAME} from namespace ${NAMESPACE}"
 
 helm uninstall --namespace ${NAMESPACE} ${RELEASE_NAME}
-kubectl --namespace ${NAMESPACE} delete secret opencontext-github-app-auth opencontext-google-cloud-storage opencontext-postgresql opencontext-postgresql-certs opencontext-postgresql-initdb opencontext-tls ${RELEASE_NAME}-opencontext-auth --now --force --wait=true
-kubectl --namespace ${NAMESPACE} delete configmap ${RELEASE_NAME}-opencontext-db-pool ${RELEASE_NAME}-opencontext-locations ${RELEASE_NAME}-opencontext-postgres-ca ${RELEASE_NAME}-opencontext-config --now --force --wait=true
-kubectl --namespace ${NAMESPACE} delete pvc data-${RELEASE_NAME}-opencontext-postgresql-0 --now --force --wait=true
+kubectl --namespace ${NAMESPACE} delete secret opencontext-github-app-auth opencontext-google-cloud-storage opencontext-postgresql opencontext-postgresql-certs opencontext-postgresql-initdb opencontext-tls ${RELEASE_NAME}-opencontext-auth --now --force
+kubectl --namespace ${NAMESPACE} delete configmap ${RELEASE_NAME}-opencontext-db-pool ${RELEASE_NAME}-opencontext-locations ${RELEASE_NAME}-opencontext-postgres-ca ${RELEASE_NAME}-opencontext-config --now --force
+kubectl --namespace ${NAMESPACE} delete pvc data-${RELEASE_NAME}-opencontext-postgresql-0 --now --force
+kubectl --namespace ${NAMESPACE} wait --for=delete pvc/data-${RELEASE_NAME}-opencontext-postgresql-0 --timeout=60s
 
 # always exit 0 since we only care if the script ran but not the exit code of each individual command
 exit 0
