1d4581dc1b288d4e8ed3b162a6a520fd91cb3d67
Alice Chen <alice@opencontext.com>
2023-04-13 17:36:18 -0700

    chore(ci): Fix permissions

--- .github/release-opencontext-version.sh
+++ .github/release-opencontext-version.sh
@@ -15,13 +15,18 @@ set -x
 
 # get opencontext app version
 OC_VERSION=$(curl -H "Authorization: token ${TOKEN}" -L -sS https://api.github.com/repos/opencontextinc/opencontext/releases/latest | jq -r .tag_name)
-CHART_APP_VERSION=$(cat charts/opencontext/Chart.yaml | grep appVersio | awk -F': ' '{print $2}')
+CHART_APP_VERSION=$(cat charts/opencontext/Chart.yaml | grep appVersion | awk -F': ' '{print $2}')
+if [[ ${OC_VERSION}zz == "zz" ]]; then
+  echo "ERROR: Could not get opencontext version"
+  exit 1
+fi
+
 if [[ ${OC_VERSION} != ${CHART_APP_VERSION} ]]; then
   echo "INFO: Updating chart appVersion ..."
   # edit opencontext chart appVersion
   ${SED:-sed} -i -E "s/^appVersion: .*/appVersion: $OC_VERSION/" charts/opencontext/Chart.yaml
   git add charts/opencontext/Chart.yaml
-  git commit -m "Updated opencontext appVersion"
+  git commit -m "ci: Updated opencontext appVersion"
   git push
 else
   echo "INFO: Chart appVersion does not need to be changed."
